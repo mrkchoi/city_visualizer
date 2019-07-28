a053958e1d825688ff1eb08c3ad63de6494286b8
Kenneth Choi <kennethichoi@gmail.com>
2019-07-28 16:05:18 -0700

    Implement map background + initial markers

---
+++ src/constants.js
@@ -0,0 +1,435 @@
+export const PLANO = "PLANO";
+const PLANO_CITY = "Plano";
+const PLANO_CLASS = "city-Plano";
+const PLANO_COLOR = "#8F94FF";
+
+export const IRVINE = "IRVINE";
+const IRVINE_CITY = "IRVINE";
+const IRVINE_CLASS = "city-Irvine";
+const IRVINE_COLOR = "#8F94FF";
+
+export const MADISON = "MADISON";
+const MADISON_CITY = "MADISON";
+const MADISON_CLASS = "city-Madison";
+const MADISON_COLOR = "#8F94FF";
+
+export const FREMONT = "FREMONT";
+const FREMONT_CITY = "FREMONT";
+const FREMONT_CLASS = "city-Fremont";
+const FREMONT_COLOR = "#8F94FF";
+
+export const HUNTINGTON_BEACH = "HUNTINGTON_BEACH";
+const HUNTINGTON_BEACH_CITY = "HUNTINGTON_BEACH";
+const HUNTINGTON_BEACH_CLASS = "city-Huntington-Beach";
+const HUNTINGTON_BEACH_COLOR = "#8F94FF";
+
+export const FARGO = "FARGO";
+const FARGO_CITY = "FARGO";
+const FARGO_CLASS = "city-Fargo";
+const FARGO_COLOR = "#8F94FF";
+
+export const GRAND_PRAIRIE = "GRAND_PRAIRIE";
+const GRAND_PRAIRIE_CITY = "GRAND_PRAIRIE";
+const GRAND_PRAIRIE_CLASS = "city-Grand-Prairie";
+const GRAND_PRAIRIE_COLOR = "#8F94FF";
+
+export const SAN_JOSE = "SAN_JOSE";
+const SAN_JOSE_CITY = "SAN_JOSE";
+const SAN_JOSE_CLASS = "city-San-Jose";
+const SAN_JOSE_COLOR = "#8F94FF";
+
+export const SCOTTSDALE = "SCOTTSDALE";
+const SCOTTSDALE_CITY = "SCOTTSDALE";
+const SCOTTSDALE_CLASS = "city-Scottsdale";
+const SCOTTSDALE_COLOR = "#8F94FF";
+
+export const SAN_FRANCISCO = "SAN_FRANCISCO";
+const SAN_FRANCISCO_CITY = "SAN_FRANCISCO";
+const SAN_FRANCISCO_CLASS = "city-San-Francisco";
+const SAN_FRANCISCO_COLOR = "#8F94FF";
+
+export const BISMARCK = "BISMARCK";
+const BISMARCK_CITY = "BISMARCK";
+const BISMARCK_CLASS = "city-Bismarck";
+const BISMARCK_COLOR = "#8F94FF";
+
+export const OVERLAND_PARK = "OVERLAND_PARK";
+const OVERLAND_PARK_CITY = "OVERLAND_PARK";
+const OVERLAND_PARK_CLASS = "city-Overland-Park";
+const OVERLAND_PARK_COLOR = "#8F94FF";
+
+export const SANTA_ROSA = "SANTA_ROSA";
+const SANTA_ROSA_CITY = "SANTA_ROSA";
+const SANTA_ROSA_CLASS = "city-Santa-Rosa";
+const SANTA_ROSA_COLOR = "#8F94FF";
+
+export const AUSTIN = "AUSTIN";
+const AUSTIN_CITY = "AUSTIN";
+const AUSTIN_CLASS = "city-Austin";
+const AUSTIN_COLOR = "#8F94FF";
+
+export const SIOUX_FALLS = "SIOUX_FALLS";
+const SIOUX_FALLS_CITY = "SIOUX_FALLS";
+const SIOUX_FALLS_CLASS = "city-Sioux-Falls";
+const SIOUX_FALLS_COLOR = "#8F94FF";
+
+export const PEARL_CITY = "PEARL_CITY";
+const PEARL_CITY_CITY = "PEARL_CITY";
+const PEARL_CITY_CLASS = "city-Pearl-City";
+const PEARL_CITY_COLOR = "#8F94FF";
+
+export const GLENDALE = "GLENDALE";
+const GLENDALE_CITY = "GLENDALE";
+const GLENDALE_CLASS = "city-Glendale";
+const GLENDALE_COLOR = "#8F94FF";
+
+export const SAN_DIEGO = "SAN_DIEGO";
+const SAN_DIEGO_CITY = "SAN_DIEGO";
+const SAN_DIEGO_CLASS = "city-San-Diego";
+const SAN_DIEGO_COLOR = "#8F94FF";
+
+export const ST_PAUL = "ST_PAUL";
+const ST_PAUL_CITY = "ST_PAUL";
+const ST_PAUL_CLASS = "city-St-Paul";
+const ST_PAUL_COLOR = "#8F94FF";
+
+export const CHARLESTON = "CHARLESTON";
+const CHARLESTON_CITY = "CHARLESTON";
+const CHARLESTON_CLASS = "city-Charleston";
+const CHARLESTON_COLOR = "#8F94FF";
+
+export const GILBERT = "GILBERT";
+const GILBERT_CITY = "GILBERT";
+const GILBERT_CLASS = "city-Gilbert";
+const GILBERT_COLOR = "#8F94FF";
+
+export const ANAHEIM = "ANAHEIM";
+const ANAHEIM_CITY = "ANAHEIM";
+const ANAHEIM_CLASS = "city-Anaheim";
+const ANAHEIM_COLOR = "#8F94FF";
+
+export const RALEIGH = "RALEIGH";
+const RALEIGH_CITY = "RALEIGH";
+const RALEIGH_CLASS = "city-Raleigh";
+const RALEIGH_COLOR = "#8F94FF";
+
+export const CAPE_CORAL = "CAPE_CORAL";
+const CAPE_CORAL_CITY = "CAPE_CORAL";
+const CAPE_CORAL_CLASS = "city-Cape-Coral";
+const CAPE_CORAL_COLOR = "#8F94FF";
+
+export const CEDAR_RAPIDS = "CEDAR_RAPIDS";
+const CEDAR_RAPIDS_CITY = "CEDAR_RAPIDS";
+const CEDAR_RAPIDS_CLASS = "city-Cedar-Rapids";
+const CEDAR_RAPIDS_COLOR = "#8F94FF";
+
+export const CITIES = {
+         PLANO: {
+           city: PLANO_CITY,
+           constant: PLANO,
+           class: PLANO_CLASS,
+           color: PLANO_COLOR,
+           emotionalWellbeing: 4,
+           incomeEmployment: 3,
+           communityEnvironment: 4,
+           position: { x: 496, y: 360 },
+           happinessScore: 72.3,
+           ranking: 1
+         },
+         IRVINE: {
+           city: IRVINE_CITY,
+           constant: IRVINE,
+           class: IRVINE_CLASS,
+           color: IRVINE_COLOR,
+           emotionalWellbeing: 8,
+           incomeEmployment: 6,
+           communityEnvironment: 3,
+           position: { x: 180, y: 300 },
+           happinessScore: 71.86,
+           ranking: 2
+         },
+         MADISON: {
+           city: MADISON_CITY,
+           constant: MADISON,
+           class: MADISON_CLASS,
+           color: MADISON_COLOR,
+           emotionalWellbeing: 2,
+           incomeEmployment: 8,
+           communityEnvironment: 4,
+           position: { x: 600, y: 160 },
+           happinessScore: 71.81,
+           ranking: 3
+         },
+         FREMONT: {
+           city: FREMONT_CITY,
+           constant: FREMONT,
+           class: FREMONT_CLASS,
+           color: FREMONT_COLOR,
+           emotionalWellbeing: 5,
+           incomeEmployment: 20,
+           communityEnvironment: 1,
+           position: { x: 151, y: 225 },
+           happinessScore: 71.17,
+           ranking: 4
+         },
+         HUNTINGTON_BEACH: {
+           city: HUNTINGTON_BEACH_CITY,
+           constant: HUNTINGTON_BEACH,
+           class: HUNTINGTON_BEACH_CLASS,
+           color: HUNTINGTON_BEACH_COLOR,
+           emotionalWellbeing: 3,
+           incomeEmployment: 25,
+           communityEnvironment: 15,
+           position: { x: 170, y: 280 },
+           happinessScore: 69.74,
+           ranking: 5
+         },
+         FARGO: {
+           city: FARGO_CITY,
+           constant: FARGO,
+           class: FARGO_CLASS,
+           color: FARGO_COLOR,
+           emotionalWellbeing: 9,
+           incomeEmployment: 11,
+           communityEnvironment: 13,
+           position: { x: 498, y: 94 },
+           happinessScore: 69.57,
+           ranking: 6
+         },
+         GRAND_PRAIRIE: {
+           city: GRAND_PRAIRIE_CITY,
+           constant: GRAND_PRAIRIE,
+           class: GRAND_PRAIRIE_CLASS,
+           color: GRAND_PRAIRIE_COLOR,
+           emotionalWellbeing: 11,
+           incomeEmployment: 19,
+           communityEnvironment: 3,
+           position: { x: 497, y: 350 },
+           happinessScore: 69.3,
+           ranking: 7
+         },
+         SAN_JOSE: {
+           city: SAN_JOSE_CITY,
+           constant: SAN_JOSE,
+           class: SAN_JOSE_CLASS,
+           color: SAN_JOSE_COLOR,
+           emotionalWellbeing: 1,
+           incomeEmployment: 10,
+           communityEnvironment: 24,
+           position: { x: 140, y: 240 },
+           happinessScore: 68.9,
+           ranking: 8
+         },
+         SCOTTSDALE: {
+           city: SCOTTSDALE_CITY,
+           constant: SCOTTSDALE,
+           class: SCOTTSDALE_CLASS,
+           color: SCOTTSDALE_COLOR,
+           emotionalWellbeing: 25,
+           incomeEmployment: 7,
+           communityEnvironment: 1,
+           position: { x: 270, y: 321 },
+           happinessScore: 68.24,
+           ranking: 9
+         },
+         SAN_FRANCISCO: {
+           city: SAN_FRANCISCO_CITY,
+           constant: SAN_FRANCISCO,
+           class: SAN_FRANCISCO_CLASS,
+           color: SAN_FRANCISCO_COLOR,
+           emotionalWellbeing: 2,
+           incomeEmployment: 1,
+           communityEnvironment: 69,
+           position: { x: 135, y: 205 },
+           happinessScore: 67.53,
+           ranking: 10
+         },
+         BISMARCK: {
+           city: BISMARCK_CITY,
+           constant: BISMARCK,
+           class: BISMARCK_CLASS,
+           color: BISMARCK_COLOR,
+           emotionalWellbeing: 14,
+           incomeEmployment: 2,
+           communityEnvironment: 25,
+           position: { x: 458, y: 106 },
+           happinessScore: 67.38,
+           ranking: 11
+         },
+         OVERLAND_PARK: {
+           city: OVERLAND_PARK_CITY,
+           constant: OVERLAND_PARK,
+           class: OVERLAND_PARK_CLASS,
+           color: OVERLAND_PARK_COLOR,
+           emotionalWellbeing: 12,
+           incomeEmployment: 31,
+           communityEnvironment: 14,
+           position: { x: 529, y: 248 },
+           happinessScore: 67.37,
+           ranking: 12
+         },
+         SANTA_ROSA: {
+           city: SANTA_ROSA_CITY,
+           constant: SANTA_ROSA,
+           class: SANTA_ROSA_CLASS,
+           color: SANTA_ROSA_COLOR,
+           emotionalWellbeing: 13,
+           incomeEmployment: 29,
+           communityEnvironment: 5,
+           position: { x: 134, y: 183 },
+           happinessScore: 67.18,
+           ranking: 13
+         },
+         AUSTIN: {
+           city: AUSTIN_CITY,
+           constant: AUSTIN,
+           class: AUSTIN_CLASS,
+           color: AUSTIN_COLOR,
+           emotionalWellbeing: 8,
+           incomeEmployment: 4,
+           communityEnvironment: 44,
+           position: { x: 477, y: 410 },
+           happinessScore: 67.16,
+           ranking: 14
+         },
+         SIOUX_FALLS: {
+           city: SIOUX_FALLS_CITY,
+           constant: SIOUX_FALLS,
+           class: SIOUX_FALLS_CLASS,
+           color: SIOUX_FALLS_COLOR,
+           emotionalWellbeing: 6,
+           incomeEmployment: 37,
+           communityEnvironment: 52,
+           position: { x: 500, y: 168 },
+           happinessScore: 66.97,
+           ranking: 15
+         },
+         PEARL_CITY: {
+           city: PEARL_CITY_CITY,
+           constant: PEARL_CITY,
+           class: PEARL_CITY_CLASS,
+           color: PEARL_CITY_COLOR,
+           emotionalWellbeing: 1,
+           incomeEmployment: 81,
+           communityEnvironment: 8,
+           position: { x: 330, y: 461 },
+           happinessScore: 66.77,
+           ranking: 16
+         },
+         GLENDALE: {
+           city: GLENDALE_CITY,
+           constant: GLENDALE,
+           class: GLENDALE_CLASS,
+           color: GLENDALE_COLOR,
+           emotionalWellbeing: 15,
+           incomeEmployment: 56,
+           communityEnvironment: 7,
+           position: { x: 167, y: 262 },
+           happinessScore: 66.25,
+           ranking: 17
+         },
+         SAN_DIEGO: {
+           city: SAN_DIEGO_CITY,
+           constant: SAN_DIEGO,
+           class: SAN_DIEGO_CLASS,
+           color: SAN_DIEGO_COLOR,
+           emotionalWellbeing: 4,
+           incomeEmployment: 20,
+           communityEnvironment: 25,
+           position: { x:187, y: 321 },
+           happinessScore: 66.01,
+           ranking: 18
+         },
+         ST_PAUL: {
+           city: ST_PAUL_CITY,
+           constant: ST_PAUL,
+           class: ST_PAUL_CLASS,
+           color: PLANO_COLOR,
+           emotionalWellbeing: 10,
+           incomeEmployment: 38,
+           communityEnvironment: 36,
+           position: { x: 555, y: 133 },
+           happinessScore: 65.79,
+           ranking: 19
+         },
+         CHARLESTON: {
+           city: CHARLESTON_CITY,
+           constant: CHARLESTON,
+           class: CHARLESTON_CLASS,
+           color: CHARLESTON_COLOR,
+           emotionalWellbeing: 41,
+           incomeEmployment: 3,
+           communityEnvironment: 2,
+           position: { x: 760, y: 366 },
+           happinessScore: 65.48,
+           ranking: 20
+         },
+         GILBERT: {
+           city: GILBERT_CITY,
+           constant: GILBERT,
+           class: GILBERT_CLASS,
+           color: GILBERT_COLOR,
+           emotionalWellbeing: 30,
+           incomeEmployment: 26,
+           communityEnvironment: 5,
+           position: { x: 277, y: 343 },
+           happinessScore: 65.07,
+           ranking: 21
+         },
+         ANAHEIM: {
+           city: ANAHEIM_CITY,
+           constant: ANAHEIM,
+           class: ANAHEIM_CLASS,
+           color: ANAHEIM_COLOR,
+           emotionalWellbeing: 16,
+           incomeEmployment: 43,
+           communityEnvironment: 19,
+           position: { x: 196, y: 282 },
+           happinessScore: 65.02,
+           ranking: 22
+         },
+         RALEIGH: {
+           city: RALEIGH_CITY,
+           constant: RALEIGH,
+           class: RALEIGH_CLASS,
+           color: RALEIGH_COLOR,
+           emotionalWellbeing: 7,
+           incomeEmployment: 23,
+           communityEnvironment: 48,
+           position: { x: 773, y: 283 },
+           happinessScore: 64.99,
+           ranking: 23
+         },
+         CAPE_CORAL: {
+           city: CAPE_CORAL_CITY,
+           constant: CAPE_CORAL,
+           class: CAPE_CORAL_CLASS,
+           color: CAPE_CORAL_COLOR,
+           emotionalWellbeing: 28,
+           incomeEmployment: 48,
+           communityEnvironment: 2,
+           position: { x: 741, y: 451 },
+           happinessScore: 64.96,
+           ranking: 24
+         },
+         CEDAR_RAPIDS: {
+           city: CEDAR_RAPIDS_CITY,
+           constant: CEDAR_RAPIDS,
+           class: CEDAR_RAPIDS_CLASS,
+           color: CEDAR_RAPIDS_COLOR,
+           emotionalWellbeing: 23,
+           incomeEmployment: 32,
+           communityEnvironment: 11,
+           position: { x: 566, y: 194 },
+           happinessScore: 64.9,
+           ranking: 25
+         }
+       };
+
+export const ANIMATION_DURATION = 1000;
+export const ANIMATION_DELAY = 0;
+export const ANIMATION_EASING = d3.easePoly;
+
+export const WIDTH = 200;
+export const HEIGHT = 200;
+export const MARGINS = 100;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // import _ from 'lodash';
+
+import Map from './map';
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Map('svg.map');
+});
